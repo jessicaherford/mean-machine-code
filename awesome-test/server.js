7e4f983bf779e02443abcd9a128717494fa7a5c5
JessicaHerford <jrherford12@gmail.com>
2016-04-06 12:47:53 -0600

    added middleware

--- awesome-test/server.js
+++ awesome-test/server.js
@@ -7,6 +7,41 @@ app.get('/', function(req, res){
   res.sendFile(path.join(__dirname + '/index.html'));
 })
 
+// create routes for the admin section
+
+// get an instance of the router
+var adminRouter = express.Router();
+
+//defines the middleware
+adminRouter.use(function(req, res, next){
+
+  // log each request to the console
+  console.log(req.method, req.url);
+
+  // continue doing what we were doing and go to the route
+  next();
+
+})
+
+// admin main page. the dashboard (http://localhost:1337/admin)
+adminRouter.get('/', function(req, res){
+  res.send('I am the dashboard!');
+})
+
+
+// users page (http://localhost:1337/admin/users)
+adminRouter.get('/users', function(req, res){
+  res.send('I show all the users!');
+})
+
+// posts page (http://localhost:1337/admin/posts)
+adminRouter.get('/posts', function(req, res){
+  res.send('I show all the posts!');
+})
+
+app.use('/admin', adminRouter);
+
+
 //start the server
 app.listen(1337);
 console.log('1337 is the magic!');
